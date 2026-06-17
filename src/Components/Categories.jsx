33579899015c67c72f15d1fc07cf22e51ef92e29
prachigharat386-sketch <prachigharat386@gmail.com>
2026-06-17 20:43:43 +0530

    add database file

---
+++ src/Components/Categories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Categories() {
+  return (
+    <section className="categories">
+      <h2>Categories</h2>
+
+      <div className="category-box">
+        <div>👗 Dresses</div>
+        <div>👜 Bags</div>
+        <div>👠 Footwear</div>
+      </div>
+    </section>
+  );
+}
+
+export default Categories;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>Fashion Vault</h2>
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Categories</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
